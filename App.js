5c0ad320b87942cfdc8a1ec388a37384490e1af0
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-09-11 11:14:18 -0400

    Added texts

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>iLostIt</Text>
+      <Text style={styles.regularText}>A simple lost and found app.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 40,
+    fontFamily: 'sans-serif',
+    marginBottom: 5,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  regularText: {
+    color:'#fff',
+  }
 });
